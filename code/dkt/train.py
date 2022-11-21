11ee7de3bc9b5eb36ca65d19cd10fd099b6c3173
NooKo <keif247@gmail.com>
2022-11-21 01:36:18 +0000

    train, inference

--- code/dkt/train.py
+++ code/dkt/train.py
@@ -18,7 +18,7 @@ def main(args):
     preprocess.load_train_data(args.file_name)
     train_data = preprocess.get_train_data()
 
-    train_data, valid_data = preprocess.split_data(train_data)
+    train_data, valid_data = preprocess.split_data(args, train_data)
 
     wandb.init(project="dkt", config=vars(args))
     model = trainer.get_model(args).to(args.device)
