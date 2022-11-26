e663f26394d178cc8da868f7c13f5f5e33434000
Kim Boseong <ses7720@gmail.com>
2022-11-26 14:35:53 +0000

    ADD wandb run name, run_with_vaild_loss [boseong]

--- code/dkt/train.py
+++ code/dkt/train.py
@@ -6,6 +6,7 @@ from args import parse_args
 from src import trainer
 from src.dataloader import Preprocess
 from src.utils import setSeeds
+from datetime import datetime
 
 
 def main(args):
@@ -20,9 +21,11 @@ def main(args):
 
     train_data, valid_data = preprocess.split_data(train_data)
 
-    wandb.init(project="dkt", config=vars(args))
+    name = datetime.now().strftime('%H%M%S')+"_"+ args.model
+    wandb.init(project="dkt", config=vars(args), name=name)
     model = trainer.get_model(args).to(args.device)
-    trainer.run(args, train_data, valid_data, model)
+    # trainer.run(args, train_data, valid_data, model)
+    trainer.run_with_vaild_loss(args, train_data, valid_data, model)
 
 
 if __name__ == "__main__":
