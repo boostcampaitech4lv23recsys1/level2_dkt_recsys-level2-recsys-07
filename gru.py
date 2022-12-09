e7eed4fa3bea26e45181d05f6e94ac5f9a0bcf8d
root <wjdgh12302@gmail.com>
2022-12-09 05:54:27 +0000

    gru~

--- gru.py
+++ gru.py
@@ -12,10 +12,15 @@ from tqdm import tqdm_notebook
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.model_selection import train_test_split  
 
+os.makedirs('level2_dkt_recsys-level2-recsys-07/output', exist_ok=True)
+
+
+
+
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-data=pd.read_csv('input/data/train_data.csv')
-test = pd.read_csv('input/data/test_data.csv')
+data=pd.read_csv('/opt/ml/new/level2_dkt_recsys-level2-recsys-07/data/train_data.csv')
+test = pd.read_csv('/opt/ml/new/level2_dkt_recsys-level2-recsys-07/data/test_data.csv')
 
 data = data[['userID','assessmentItemID','testId','Timestamp','KnowledgeTag','answerCode']]
 test = test[['userID','assessmentItemID','testId','Timestamp','KnowledgeTag','answerCode']]
@@ -36,14 +41,14 @@ X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
 print("Training Shape", X_train.shape, y_train.shape)
 print("Testing Shape", X_test.shape, y_test.shape)
 
-X_train_tensors = Variable(torch.Tensor(X_train.values))
-X_test_tensors = Variable(torch.Tensor(X_test.values))
+X_train_tensors = Variable(torch.Tensor(X_train.values)).to(device)
+X_test_tensors = Variable(torch.Tensor(X_test.values)).to(device)
 
-y_train_tensors = Variable(torch.Tensor(y_train.values))
-y_test_tensors = Variable(torch.Tensor(y_test.values))
+y_train_tensors = Variable(torch.Tensor(y_train.values)).to(device)
+y_test_tensors = Variable(torch.Tensor(y_test.values)).to(device)
 
-X_train_tensors_f = torch.reshape(X_train_tensors,   (X_train_tensors.shape[0], 1, X_train_tensors.shape[1]))
-X_test_tensors_f = torch.reshape(X_test_tensors,  (X_test_tensors.shape[0], 1, X_test_tensors.shape[1])) 
+X_train_tensors_f = torch.reshape(X_train_tensors,   (X_train_tensors.shape[0], 1, X_train_tensors.shape[1])).to(device)
+X_test_tensors_f = torch.reshape(X_test_tensors,  (X_test_tensors.shape[0], 1, X_test_tensors.shape[1])).to(device)
 
 print("Training Shape", X_train_tensors_f.shape, y_train_tensors.shape)
 print("Testing Shape", X_test_tensors_f.shape, y_test_tensors.shape)
@@ -51,37 +56,37 @@ print("Testing Shape", X_test_tensors_f.shape, y_test_tensors.shape)
 class GRU(nn.Module):
     def __init__(self, num_classes, input_size, hidden_size, num_layers, seq_length):
         super(GRU, self).__init__()
-        self.num_classes = num_classes 
-        self.num_layers = num_layers 
-        self.input_size = input_size 
-        self.hidden_size = hidden_size 
-        self.seq_length = seq_length 
+        self.num_classes = num_classes
+        self.num_layers = num_layers
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.seq_length = seq_length
 
         self.gru = nn.GRU(input_size=input_size, hidden_size=hidden_size,
-                          num_layers=num_layers, batch_first=True) 
-        self.fc_1 =  nn.Linear(hidden_size, 128) 
-        self.fc = nn.Linear(128, num_classes) 
-        self.relu = nn.ReLU()
+                          num_layers=num_layers, batch_first=True).to(device)
+        self.fc_1 =  nn.Linear(hidden_size, 128).to(device)
+        self.fc = nn.Linear(128, num_classes).to(device)
+        self.relu = nn.ReLU().to(device)
     
     def forward(self,x):
-        h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)) 
-        output, (hn) = self.gru(x, (h_0)) 
-        hn = hn.view(-1, self.hidden_size) 
-        out = self.relu(hn)
-        out = self.fc_1(out) 
-        out = self.relu(out) 
-        out = self.fc(out) 
+        h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).to(device)
+        output, (hn) = self.gru(x, (h_0))
+        hn = hn.view(-1, self.hidden_size).to(device) 
+        out = self.relu(hn).to(device)
+        out = self.fc_1(out).to(device)
+        out = self.relu(out).to(device)
+        out = self.fc(out).to(device)
         return out
 
-num_epochs = 1000 
-learning_rate = 0.0001 
+num_epochs = 200
+learning_rate = 0.1 
 
 input_size = 2 
-hidden_size = 2 
+hidden_size = 64
 num_layers = 1 
 
 num_classes = 1 
-model = GRU(num_classes, input_size, hidden_size, num_layers, X_train_tensors_f.shape[1]) 
+model = GRU(num_classes, input_size, hidden_size, num_layers, X_train_tensors_f.shape[1]).to(device)
 
 criterion = torch.nn.MSELoss()    
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) 
@@ -106,3 +111,22 @@ for epoch in range(num_epochs):
 # X_train_tensors_f = torch.reshape(X_train_tensors,   (X_train_tensors.shape[0], 1, X_train_tensors.shape[1]))
 # X_test_tensors_f = torch.reshape(X_test_tensors,  (X_test_tensors.shape[0], 1, X_test_tensors.shape[1])) 
 
+
+X_t=test.iloc[:,:-1]
+y_t=test.iloc[:,-1:]
+
+X_t_tensors = Variable(torch.Tensor(X_t.values)).to(device)
+y_t_tensors = Variable(torch.Tensor(y_t.values)).to(device)
+
+X_t_tensors_f = torch.reshape(X_t_tensors,   (X_t_tensors.shape[0], 1, X_t_tensors.shape[1])).to(device)
+
+
+print("Training Shape", X_t_tensors_f.shape, y_t_tensors.shape)
+
+
+predict = model(X_t_tensors_f).to(device)
+predicted = predict.cpu().detach().numpy()
+print(predicted)
+print(len(predicted))
+submission = pd.DataFrame(predicted)
+submission.to_csv('/opt/ml/new/level2_dkt_recsys-level2-recsys-07/output/aaasubmission.csv')
