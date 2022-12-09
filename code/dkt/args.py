4e324ba22b1178918684c8b22d774c83fa2b3553
Kim Boseong <ses7720@gmail.com>
2022-12-09 05:20:06 +0000

    ADD HPO [boseng]

--- code/dkt/args.py
+++ code/dkt/args.py
@@ -19,7 +19,7 @@ def parse_args():
     )
 
     parser.add_argument(
-        "--file_name", default="train_data.csv", type=str, help="train file name"
+        "--file_name", default="train_data2.csv", type=str, help="train file name"
     )
 
     parser.add_argument(
@@ -37,7 +37,7 @@ def parse_args():
     )
 
     parser.add_argument(
-        "--max_seq_len", default=20, type=int, help="max sequence length"
+        "--max_seq_len", default=5, type=int, help="max sequence length"
     )
     parser.add_argument("--num_workers", default=1, type=int, help="number of workers")
 
@@ -50,18 +50,18 @@ def parse_args():
     parser.add_argument("--drop_out", default=0.2, type=float, help="drop out rate")
 
     # 훈련
-    parser.add_argument("--n_epochs", default=20, type=int, help="number of epochs")
-    parser.add_argument("--batch_size", default=64, type=int, help="batch size")
+    parser.add_argument("--n_epochs", default=50, type=int, help="number of epochs")
+    parser.add_argument("--batch_size", default=16, type=int, help="batch size")
     parser.add_argument("--lr", default=0.0001, type=float, help="learning rate")
     parser.add_argument("--clip_grad", default=10, type=int, help="clip grad")
-    parser.add_argument("--patience", default=5, type=int, help="for early stopping")
+    parser.add_argument("--patience", default=50, type=int, help="for early stopping")
 
     parser.add_argument(
         "--log_steps", default=50, type=int, help="print log per n steps"
     )
 
     ### 중요 ###
-    parser.add_argument("--model", default="lstm", type=str, help="model type")
+    parser.add_argument("--model", default="LSTM", type=str, help="model type")
     parser.add_argument("--optimizer", default="adam", type=str, help="optimizer type")
     parser.add_argument(
         "--scheduler", default="plateau", type=str, help="scheduler type"
